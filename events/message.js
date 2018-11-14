e134d267001de43b7ce5b76d96c2db983107c5d9
Velikss <felixbeuving@live.nl>
2018-11-14 17:16:17 +0100

    First update

---
+++ events/message.js
@@ -0,0 +1,28 @@
+﻿const config = require(`../config.json`);
+
+exports.run = (client, message) => {
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+
+    try {
+        let genFile = require(`../commands/${command}.js`);
+        genFile.run(client, message, args);
+    } catch (err) {
+        try {
+            let utlFile = require(`../commands/general/${command}.js`);
+            utlFile.run(client, message, args);
+        } catch (err) {
+            try {
+                let utlFile = require(`../commands/utilities/${command}.js`);
+                utlFile.run(client, message, args);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+};
---
+++ events/ready.js
@@ -0,0 +1,6 @@
+﻿const Discord = require(`discord.js`);
+const config = require(`../config.json`);
+exports.run = (client) => {
+    console.log(`[Bot is online | Node: ${process.version} | Discord.js: ${Discord.version}]\nConnected as: ${client.user.username} (ID: ${client.user.id})\nGuilds Connected: ${client.guilds.size}`);
+    client.user.setActivity(`${client.guilds.size} Servers | ${config.prefix}help`, { type: 'WATCHING' });
+};
