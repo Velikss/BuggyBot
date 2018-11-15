f168a004deb66f58055404c54951c51492464421
Velikss <felixbeuving@live.nl>
2018-11-15 20:49:44 +0100

    Hotfix to eventhandler

--- buggy.js
+++ buggy.js
@@ -1,25 +1,26 @@
 const Discord = require(`discord.js`);
-const client = new Discord.Client();
 const fs = require("fs");
-const config = require("./config.json");
 const mysql = require("mysql");
+
+const client = new Discord.Client();
+client.config = require("./config.json");
 client.logger = require("./functions/logger");
 
 var con = mysql.createConnection({
-  host: config.sqlhost,
-  user: config.sqluser,
-  password: config.sqlpassword,
-  database: config.sqldatabase
+  host: client.config.sqlhost,
+  user: client.config.sqluser,
+  password: client.config.sqlpassword,
+  database: client.config.sqldatabase
 });
 
 fs.readdir(`./events/`,
     (err, files) => {
         if (err) return client.logger.error(err);
         files.forEach(file => {
-            const eventFunction = require(`./events/${file}`);
+            const event = require(`./events/${file}`);
             const eventName = file.split(".")[0];
-            client.on(eventName, (...args) => eventFunction.run(client, con, ...args));
+            client.on(eventName, (...args) => event.run(client, ...args, con));
         });
     });
 
-client.login(config.token);
+client.login(client.config.token);
