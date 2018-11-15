8258b57c5c1851b234721c8571aa4a5dcab2718c
Velikss <felixbeuving@live.nl>
2018-11-15 19:23:54 +0100

    Added SQL database

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,14 @@
 ﻿const Discord = require(`discord.js`);
 const config = require("../config.json");
 
-exports.run = (client) => {
+exports.run = (client, con) => {
     client.logger.log(
         `[Bot is online | Node: ${process.version} | Discord.js: ${Discord.version}]\nConnected as: ${
         client.user.username} (ID: ${client.user.id})\nGuilds Connected: ${client.guilds.size}`,
         "ready");
     client.user.setActivity(`you`, { type: "WATCHING" });
+    con.connect(err => {
+      if(err) throw err;
+      client.logger.log("Connected to database!" ,"ready");
+    });
 };
