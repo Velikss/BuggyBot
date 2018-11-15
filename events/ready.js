ca3f35c716a9acfa3cca9eb2064cbba07ad3c298
Velikss <felixbeuving@live.nl>
2018-11-15 22:12:26 +0100

    Changed ready log

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,7 @@
 const config = require("../config.json");
 
 exports.run = (client, con) => {
-    client.logger.log(
-        `[Bot is online | Node: ${process.version} | Discord.js: ${Discord.version}]\nConnected as: ${
-        client.user.username} (ID: ${client.user.id})\nGuilds Connected: ${client.guilds.size}`,
-        "ready");
+    client.logger.log(`Bot is online | Connected as: ${client.user.username} | Guilds: ${client.guilds.size}`,"ready");
     client.user.setActivity(`you`, { type: "WATCHING" });
     con.connect(err => {
       if(err) throw err;
