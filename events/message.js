3cb7fbd00070401f6884b3a1c3e54077775d6341
Velikss <felixbeuving@live.nl>
2018-11-16 21:53:55 +0100

    Small changes

--- events/message.js
+++ events/message.js
@@ -1,15 +1,13 @@
-﻿const config = require("../config.json");
-
-exports.run = (client, message) => {
+﻿exports.run = (client, message) => {
     if (message.author.bot) return;
 
     if (message.channel.type === "dm") {
         client.logger.log(`${message.author.tag} sent ${message.content}`, "dm");
         return;
     }
-    if (message.content.indexOf(config.prefix) !== 0) return;
+    if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     const cmd = client.commands.get(command);
--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,4 @@
-﻿const Discord = require(`discord.js`);
-
 exports.run = (client) => {
-
     client.user.setActivity(`you`, { type: "WATCHING" });
-
     client.logger.log(`Bot is online | Connected as: ${client.user.username} | Guilds: ${client.guilds.size}`,"ready");
 };
