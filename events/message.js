91bf9d1228048bfb553671dece101f60abed628e
Velikss <felixbeuving@live.nl>
2018-12-06 20:49:40 +0100

    Commands now work in DMs

--- events/message.js
+++ events/message.js
@@ -1,11 +1,11 @@
 ﻿exports.run = (client, message) => {
     if (message.author.bot) return;
 
+    if (message.content.indexOf(client.config.prefix) !== 0) return;
+
     if (message.channel.type === "dm") {
         client.logger.log(`${message.author.tag} sent ${message.content}`, "dm");
-        return;
     }
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
 
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
