89a922f687d9f51bee79aba96402950ed3e46036
Felix <felixbeuving@live.nl>
2019-10-04 13:50:06 +0200

    Needs SQL

--- commands/add-sound.js
+++ commands/add-sound.js
@@ -2,25 +2,24 @@ exports.run = (client, message, args) => {
   if(!args[1] ) {
     message.reply("Not enough arguments given.");
   } else {
-    id = args[1];
-    url = args[2];
+    id = args[0];
+    url = args[1];
 
     //ID is already registered
-    if (client.config.userGreetings.hasOwnProperty(id))
-      client.config.userGreeting[id].url = "default";
-      client.fs.writeFile("../config.json", JSON.stringify(client.config), (err) => console.error);
+
+    //ID is not registered yet
 
 
   }
 };
 
 exports.conf = {
-  aliases: ["h2", "halp2"]
+  aliases: ["add-greeting"]
 };
 
 exports.help = {
   name: "add-sound",
-  category: "General",
-  description: "Displays all the available commands.",
+  category: "Sound",
+  description: "Add greeting sound-effect",
   usage: "add-sound [command]"
 };
